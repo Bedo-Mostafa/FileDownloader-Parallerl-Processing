849852f6bec8ff5b8f91a4f1ec0e15c60da4ac35
Bedo-Mostafa <141512177+Bedo-Mostafa@users.noreply.github.com>
2024-12-07 01:21:32 +0200

    initial commit

---
+++ Parallerl-Processing/Parallerl-Processing.FileDownloader/Form1.cs
@@ -0,0 +1,10 @@
+namespace Parallerl_Processing.FileDownloader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
